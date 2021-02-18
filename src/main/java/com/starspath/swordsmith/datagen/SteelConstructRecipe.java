3184748e082bf6198b205b5aade1f1b0fd111b56
StarsPath <39964463+StarsPath@users.noreply.github.com>
2021-02-17 22:30:47 -0500

    Fixed recipe bug

--- src/main/java/com/starspath/swordsmith/datagen/SteelConstructRecipe.java
+++ src/main/java/com/starspath/swordsmith/datagen/SteelConstructRecipe.java
@@ -3,8 +3,10 @@ package com.starspath.swordsmith.datagen;
 import com.google.gson.JsonObject;
 import com.starspath.swordsmith.SwordSmith;
 import com.starspath.swordsmith.core.init.ItemInit;
+import com.starspath.swordsmith.core.init.RecipeInit;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.AirItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.ICraftingRecipe;
 import net.minecraft.item.crafting.IRecipeSerializer;
@@ -19,8 +21,6 @@ import java.util.ArrayList;
 
 public class SteelConstructRecipe implements ICraftingRecipe {
 
-    private final ItemStack steelConstruct = new ItemStack(ItemInit.STEEL_CONSTRUCT.get());
-
     private final ResourceLocation id;
 
     public SteelConstructRecipe(ResourceLocation idIn){
@@ -29,9 +29,10 @@ public class SteelConstructRecipe implements ICraftingRecipe {
 
     @Override
     public boolean matches(CraftingInventory inv, World worldIn) {
-        ArrayList<ItemStack> itemList = new ArrayList<ItemStack>();
         boolean shingane = false;
         boolean kawagane = false;
+        int sCount = 0;
+        int kCount = 0;
 
         for(int i = 0; i < inv.getSizeInventory(); i++) {
             ItemStack itemStack = inv.getStackInSlot(i);
@@ -40,19 +41,24 @@ public class SteelConstructRecipe implements ICraftingRecipe {
             CompoundNBT nbt = itemStack.getOrCreateTag();
 
             if(itemStack.getItem() == ItemInit.SHINGANE_STACK.get() &&
-                    nbt.getInt("ForgeStage") == 0 && nbt.getInt("HeatStage") > 0)
+                    nbt.getInt("ForgeStage") == 0 && nbt.getInt("HeatStage") > 0){
                 shingane = true;
+                sCount += 1;
+            }
             if(itemStack.getItem() == ItemInit.KAWAGANE_STACK.get() &&
-                    nbt.getInt("ForgeStage") == 2 && nbt.getInt("HeatStage") > 0)
+                    nbt.getInt("ForgeStage") == 2 && nbt.getInt("HeatStage") > 0){
                 kawagane = true;
-            itemList.add(itemStack);
+                kCount += 1;
+            }
         }
-        return itemList.size() == 2 && shingane && kawagane;
+        boolean decision = shingane && kawagane && sCount == 1 && kCount == 1;
+        System.out.print(decision);
+        return decision;
     }
 
     @Override
     public ItemStack getCraftingResult(CraftingInventory inv) {
-        return steelConstruct;
+        return new ItemStack(ItemInit.STEEL_CONSTRUCT.get());
     }
 
     @Override
@@ -62,7 +68,7 @@ public class SteelConstructRecipe implements ICraftingRecipe {
 
     @Override
     public ItemStack getRecipeOutput() {
-        return steelConstruct;
+        return new ItemStack(ItemInit.STEEL_CONSTRUCT.get());
     }
 
     @Override
@@ -72,7 +78,7 @@ public class SteelConstructRecipe implements ICraftingRecipe {
 
     @Override
     public IRecipeSerializer<?> getSerializer() {
-        return null;
+        return RecipeInit.STEEL_CONSTRUCT_RECIPE.get();
     }
 
     public static class Serializer extends ForgeRegistryEntry<IRecipeSerializer<?>> implements IRecipeSerializer<SteelConstructRecipe>{
